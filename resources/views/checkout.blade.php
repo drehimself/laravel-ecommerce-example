34ec372871b5fedae89cef340f2a6c829d6f43c6
Andre Madarang <andre_madarang@hotmail.com>
2017-11-15 02:18:55 -0500

    change name to name on card

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -56,8 +56,8 @@
                     <h2>Payment Details</h2>
 
                     <div class="form-group">
-                        <label for="name">Name</label>
-                        <input type="text" class="form-control" id="name" name="name" value="">
+                        <label for="name_on_card">Name on Card</label>
+                        <input type="text" class="form-control" id="name_on_card" name="name_on_card" value="">
                     </div>
                     <div class="form-group">
                         <label for="address">Address</label>
