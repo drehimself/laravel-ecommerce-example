c921d036b11bb21b4060765d25a9134645f3632f
Andre Madarang <andre_madarang@hotmail.com>
2018-01-20 00:42:06 -0500

    Part 7 - Deploy to Forge

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -38,8 +38,8 @@
                     <h1>Laravel Ecommerce Demo</h1>
                     <p>Includes multiple products, categories, a shopping cart and a checkout system with Stripe integration.</p>
                     <div class="hero-buttons">
-                        <a href="#" class="button button-white">Blog Post</a>
-                        <a href="#" class="button button-white">GitHub</a>
+                        <a href="https://www.youtube.com/playlist?list=PLEhEHUEU3x5oPTli631ZX9cxl6cU_sDaR" class="button button-white">Screencast</a>
+                        <a href="https://github.com/drehimself/laravel-ecommerce-example" class="button button-white">GitHub</a>
                     </div>
                 </div> <!-- end hero-copy -->
 
