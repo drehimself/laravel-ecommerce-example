bdf57a5f17b237919144986e09946e9bb536a45d
Andre Madarang <andre_madarang@hotmail.com>
2018-02-02 14:21:00 -0500

    change migration class name

--- database/migrations/2018_01_11_060124_create_category_table.php
+++ database/migrations/2018_01_11_060124_create_category_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoriesTable extends Migration
+class CreateCategoryTable extends Migration
 {
     /**
      * Run the migrations.
