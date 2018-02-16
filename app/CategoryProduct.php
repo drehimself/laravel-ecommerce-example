bf4e32d06ee9b4d0c5c373188df9dc7d1fbe1704
Andre Madarang <andre_madarang@hotmail.com>
2018-02-15 19:09:22 -0500

    Part 16 - Customizing Views & Controllers in Voyager

--- app/CategoryProduct.php
+++ app/CategoryProduct.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class CategoryProduct extends Model
 {
     protected $table = 'category_product';
+
+    protected $fillable = ['product_id', 'category_id'];
 }
