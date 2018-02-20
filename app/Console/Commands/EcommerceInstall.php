431c61a297dda14aa07372126cf7b02a27696b6a
Andre Madarang <andre_madarang@hotmail.com>
2018-02-20 00:38:33 -0500

    force seeds in production

--- app/Console/Commands/EcommerceInstall.php
+++ app/Console/Commands/EcommerceInstall.php
@@ -58,50 +58,62 @@ class EcommerceInstall extends Command
 
         $this->call('migrate:fresh', [
             '--seed' => true,
+            '--force' => true,
         ]);
 
         $this->call('db:seed', [
             '--class' => 'VoyagerDatabaseSeeder',
+            '--force' => true,
         ]);
 
         $this->call('db:seed', [
             '--class' => 'VoyagerDummyDatabaseSeeder',
+            '--force' => true,
         ]);
 
         $this->call('db:seed', [
             '--class' => 'DataTypesTableSeederCustom',
+            '--force' => true,
         ]);
 
         $this->call('db:seed', [
             '--class' => 'DataRowsTableSeederCustom',
+            '--force' => true,
         ]);
 
         $this->call('db:seed', [
             '--class' => 'MenusTableSeederCustom',
+            '--force' => true,
         ]);
 
         $this->call('db:seed', [
             '--class' => 'MenuItemsTableSeederCustom',
+            '--force' => true,
         ]);
 
         $this->call('db:seed', [
             '--class' => 'RolesTableSeederCustom',
+            '--force' => true,
         ]);
 
         $this->call('db:seed', [
             '--class' => 'PermissionsTableSeederCustom',
+            '--force' => true,
         ]);
 
         $this->call('db:seed', [
             '--class' => 'PermissionRoleTableSeeder',
+            '--force' => true,
         ]);
 
         $this->call('db:seed', [
             '--class' => 'PermissionRoleTableSeederCustom',
+            '--force' => true,
         ]);
 
         $this->call('db:seed', [
             '--class' => 'UsersTableSeederCustom',
+            '--force' => true,
         ]);
 
         $this->info('Dummy data installed');
