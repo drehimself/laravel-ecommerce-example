85a606baa80c09cfd53527b39c29405f4afac69d
Andre Madarang <andre_madarang@hotmail.com>
2018-03-09 00:45:25 -0500

    fix search JS on cart and product page

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -189,9 +189,7 @@
             })
         })();
     </script>
-@endsection
 
-@section('extra-js')
     <!-- Include AlgoliaSearch JS Client and autocomplete.js library -->
     <script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
     <script src="https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.min.js"></script>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -100,11 +100,10 @@
 
         })();
     </script>
-@endsection
 
-@section('extra-js')
     <!-- Include AlgoliaSearch JS Client and autocomplete.js library -->
     <script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
     <script src="https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.min.js"></script>
     <script src="{{ asset('js/algolia.js') }}"></script>
+
 @endsection
