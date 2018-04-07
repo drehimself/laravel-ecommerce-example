2729d52b40886580219038a570d271016070ea31
Andre Madarang <andre_madarang@hotmail.com>
2018-04-07 03:40:57 -0400

    Fixes #5. Catch exception if Algolia credentials incorrect

--- app/Console/Commands/EcommerceInstall.php
+++ app/Console/Commands/EcommerceInstall.php
@@ -65,10 +65,14 @@ class EcommerceInstall extends Command
         File::copyDirectory(public_path('img/posts'), public_path('storage/posts'));
         File::copyDirectory(public_path('img/users'), public_path('storage/users'));
 
-        $this->call('migrate:fresh', [
-            '--seed' => true,
-            '--force' => true,
-        ]);
+        try {
+            $this->call('migrate:fresh', [
+                '--seed' => true,
+                '--force' => true,
+            ]);
+        } catch (\Exception $e) {
+            $this->error('Algolia credentials incorrect. Your products table is NOT seeded correctly. If you are not using Algolia, remove Searchable trait from App\Product');
+        }
 
         $this->call('db:seed', [
             '--class' => 'VoyagerDatabaseSeeder',
@@ -125,13 +129,17 @@ class EcommerceInstall extends Command
             '--force' => true,
         ]);
 
-        $this->call('scout:clear', [
-            'model' => 'App\\Product',
-        ]);
+        try {
+            $this->call('scout:clear', [
+                'model' => 'App\\Product',
+            ]);
 
-        $this->call('scout:import', [
-            'model' => 'App\\Product',
-        ]);
+            $this->call('scout:import', [
+                'model' => 'App\\Product',
+            ]);
+        } catch (\Exception $e) {
+            $this->error('Algolia credentials incorrect. Check your .env file.');
+        }
 
         $this->info('Dummy data installed');
     }
