f6e0bfd34d0c6c30e984881ef757e9c19e3efd89
saber <saber.tabatabaee@gmail.com>
2021-05-07 01:03:40 +0430

    translate forget password page

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -17,24 +17,24 @@
                 </ul>
             </div>
             @endif
-            <h2>Forgot Password?</h2>
+            <h2>{{ __('site.forgetpassword.forgetpassword') }}</h2>
             <div class="spacer"></div>
             <form action="{{ route('password.email') }}" method="POST">
                 {{ csrf_field() }}
-                <input type="email" id="email" name="email" value="{{ old('email') }}" placeholder="Email" required autofocus>
+                <input type="email" id="email" name="email" value="{{ old('email') }}" placeholder="{{ __('site.forgetpassword.email') }}" required autofocus>
                 <div class="login-container">
-                    <button type="submit" class="auth-button">Send Password Reset Link</button>
+                    <button type="submit" class="auth-button">{{ __('site.forgetpassword.sendPasswordResetLink') }}</button>
                 </div>
 
 
             </form>
         </div>
         <div class="auth-right">
-            <h2>Forgotten Password Information</h2>
+            <h2>{{ __('site.forgetpassword.forgottenPasswordInformation') }}</h2>
             <div class="spacer"></div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel dicta obcaecati exercitationem ut atque inventore cum. Magni autem error ut!</p>
+            <p>{{ __('site.forgetpassword.text1') }}</p>
             <div class="spacer"></div>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel accusantium quasi necessitatibus rerum fugiat eos, a repudiandae tempore nisi ipsa delectus sunt natus!</p>
+            <p>{{ __('site.forgetpassword.text2') }}</p>
         </div>
     </div>
 </div>
